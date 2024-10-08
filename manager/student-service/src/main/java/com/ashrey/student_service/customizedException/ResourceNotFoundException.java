a026978eb8437d0387d8ca450ccdd16509bc4ef5
Rishav <rishavdadwal1@gmail.com>
2024-10-08 15:27:41 +0530

    removed Spring old manger added new one fresh

--- manager/student-service/src/main/java/com/ashrey/student_service/customizedException/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ashrey.student_service.customizedException;
-
-public class ResourceNotFoundException extends Exception {
-    public ResourceNotFoundException(String errorMessage) {
-        super(errorMessage);
-    }
-}
