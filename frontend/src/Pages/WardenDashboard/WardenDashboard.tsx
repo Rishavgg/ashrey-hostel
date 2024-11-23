6f6a51a1081e2b6cd0fd6a6641a91119e5426227
Nikhilesh <98324253+N-ikhilesh@users.noreply.github.com>
2024-11-23 18:08:16 +0530

    added student dashboard and warden dashboard -u

--- frontend/src/Pages/WardenDashboard/WardenDashboard.tsx
+++ frontend/src/Pages/WardenDashboard/WardenDashboard.tsx
@@ -1,37 +1,56 @@
 // import OutpassCard from '../../components/OutpassCard.tsx'
 import Navbar from '../../components/NavbarWarden.tsx'
 import FilterBar from '../../components/FilterBar.tsx'
+import NameCard from '../../components/NameCard.tsx';
 
 
 const HomePage = () => {
     return (
         <div>
-            {/* <OutpassCard
-                name="John Doe"
-                id="93890234729"
-                status="3rd"
-                year="H15 B9"
-                placeOfVisit="Place of visit"
-                reasonForVisit="Reason for visit"
-                leaveDate="21/11/2023"
-                returnDate="25/11/2023"
-                daysDifference={4}
-            /> */}
-            {/* <div>      
-            <div style={{display:'flex',flexDirection:'row'}}>
-            <div style={{width:'25%'}}><Navbar/></div>
-            <div style={{width:'75%'}}><FilterBar title='page title' /></div>
-            
-            </div> */}
-
-
-
 
         <div style={{display:'flex',flexDirection:'row'}}>
             <Navbar/>
             <div style={{display:'flex',flexDirection:'column',maxWidth:'79vw',width:'100%',minWidth:'fit-content'}} >
                 <FilterBar title='page title' />
-                <div style={{padding:'20px', gap:'20px',position:'sticky',width:'100%'}}>rest of the content here</div>
+                <div style={{padding:'20px', gap:'20px',position:'sticky',width:'100%',display:'flex',flexWrap:'wrap'}}>
+                    
+                    <NameCard
+                        name="Jhon Doe"
+                        id= "211478"
+                        status="H 15 B9"
+                        year="2nd"
+                        // feesStatus='u'
+                    />
+
+                    <NameCard
+                        name="Jhon Doe"
+                        id= "211478"
+                        status="H 15 B9"
+                        year="2nd"
+                        // feesStatus='u'
+                        />
+                        
+                    <NameCard
+                        name="Jhon Doe"
+                        id= "211478"
+                        status="H 15 B9"
+                        year="2nd"
+                        // feesStatus='u'
+                    /><NameCard
+                    name="Jhon Doe"
+                    id= "211478"
+                    status="H 15 B9"
+                    year="2nd"
+                    // feesStatus='u'
+                    /><NameCard
+                    name="Jhon Doe"
+                    id= "211478"
+                    status="H 15 B9"
+                    year="2nd"
+                    // feesStatus='u'
+                />
+                    
+                    </div>
                 
                 </div>
         </div>
