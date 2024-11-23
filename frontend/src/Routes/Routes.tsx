a0097711a0aae4f5eccddb4334f1c5f9411efe87
Nikhilesh <98324253+N-ikhilesh@users.noreply.github.com>
2024-11-23 17:59:38 +0530

    added student dashboard and warden dashboard -u

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -3,7 +3,11 @@ import App from "../App";
 import LoginPage from "../Pages/LoginPage/LoginPage";
 import Navbar from "../components/NavbarWarden.tsx";
 import ResetPage from "../Pages/ResetPage/ResetPage.tsx"
-import Temp from "../Pages/Temp/Temp.tsx"
+import Temp from "../Pages/temp/temp.tsx"
+import Warden from "../Pages/WardenDashboard/WardenDashboard.tsx"
+import Student from "../Pages/StudentDashboard/StudentDashboard.tsx"
+
+
 
 
 export const router = createBrowserRouter([
@@ -16,6 +20,8 @@ export const router = createBrowserRouter([
             { path: 'navbar', element: <Navbar/> },
             { path: 'reset', element: <ResetPage/> },
             { path: 'temp', element: <Temp/> },
+            { path: 'warden', element: <Warden/> },
+            { path: 'student', element: <Student/> },
         ],
     },
 ]);
