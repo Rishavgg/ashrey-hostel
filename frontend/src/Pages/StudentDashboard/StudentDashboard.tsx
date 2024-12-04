505f37ff71c0c8c800d04973e809c9c99a8f24ca
Nikhilesh <98324253+N-ikhilesh@users.noreply.github.com>
2024-12-05 02:28:39 +0530

    Student Dashboard update

--- frontend/src/Pages/StudentDashboard/StudentDashboard.tsx
+++ frontend/src/Pages/StudentDashboard/StudentDashboard.tsx
@@ -1,78 +1,48 @@
-// import OutpassCard from '../../components/OutpassCard.tsx'
-import Navbar from '../../components/NavbarStudent.tsx'
+import Navbar from '../../components/NavbarStudent.tsx';
 import { useState } from "react";
 
-// pages
-import ListOfRooms from "./ListOfRooms.tsx"
-import InventoryForm from "./InventoryForm.tsx"
-import ParticipateInMassAllocation from "./ParticipateInMassAllocation.tsx"
-import RegisterComplaint from "./RegisterComplaint.tsx"
-import RequestRoomChange from "./RequestRoomChange.tsx"
-
-// import FilterBar from '../../components/FilterBar.tsx'
-
+// Pages
+import ListOfRooms from "./ListOfRooms.tsx";
+import InventoryForm from "./InventoryForm.tsx";
+import ParticipateInMassAllocation from "./ParticipateInMassAllocation.tsx";
+import RegisterComplaint from "./RegisterComplaint.tsx";
+import RequestRoomChange from "./RequestRoomChange.tsx";
 
 const HomePage = () => {
+  const [activePage, setActivePage] = useState<string>('List of Rooms');
 
-    const [activePage, setActivePage] = useState<string>('List of Rooms'); // Default page
-    // Callback for changing the active page
-    const handlePageChange = (page: string) => {
-      setActivePage(page);
-    };
+  const handlePageChange = (page: string) => {
+    setActivePage(page);
+  };
 
-    const renderContent = () => {
-        switch (activePage) {
-          case 'List of Rooms':
-            return <ListOfRooms />;
-          case 'Inventory Form':
-            return <InventoryForm/>;
-          case 'Participate in Mass Allocation':
-            return <ParticipateInMassAllocation />;
-          case 'Regsiter A Complaint':
-            return <RegisterComplaint/>;
-          case 'Request Room change':
-            return <RequestRoomChange/>;
-          default:
-            return <ListOfRooms />;
-        }
-      };
+  const renderContent = () => {
+    switch (activePage) {
+      case 'List of Rooms':
+        return <ListOfRooms />;
+      case 'Inventory Form':
+        return <InventoryForm />;
+      case 'Participate in Mass Allocation':
+        return <ParticipateInMassAllocation />;
+      case 'Register A Complaint':
+        return <RegisterComplaint />;
+      case 'Request Room change':
+        return <RequestRoomChange />;
+      default:
+        return <ListOfRooms />;
+    }
+  };
 
+  return (
+    <div style={{ display: 'flex', height: '100vh', overflow: 'hidden' }}>
+      {/* Navbar */}
+      <Navbar onPageChange={handlePageChange} />
 
-    return (
-        <div>
-            <div
-                style={{
-                display: 'flex',
-                flexDirection: 'row',
-                height: '100vh',
-                overflow: 'hidden',
-                }}
-            >
-                {/* Navbar */}
-                <Navbar onPageChange={handlePageChange} /> {/* Pass callback to Navbar */}
-
-                {/* Main Content */}
-                <div
-                style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    flexGrow: 1,
-                    maxWidth: '79%',
-                    minWidth: '0'
-                }}
-                >
-                {/* Dynamic Content Area */}
-                <div
-                    style={{
-                    width: '100%',
-                    }}
-                >
-                    {renderContent()} {/* Render the content based on activePage */}
-                </div>
-                </div>
-            </div>
-        </div>
-    );
+      {/* Main Content */}
+      <div style={{ flexGrow: 1, maxWidth: '79%', minWidth: '0' }}>
+        <div style={{ width: '100%' }}>{renderContent()}</div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
