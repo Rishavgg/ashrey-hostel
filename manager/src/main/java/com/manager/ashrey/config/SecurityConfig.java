0fb8b932024a92cea1e11c7b07374c4088710173
root <root@DESKTOP-3JCKC3F.>
2025-05-11 08:40:07 +0000

    fix security  in manager

--- manager/src/main/java/com/manager/ashrey/config/SecurityConfig.java
+++ manager/src/main/java/com/manager/ashrey/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
                         "http://localhost:5173",
                         "http://localhost:3000",
                         "http://localhost:8080",
-                        "http://localhost:8081",
+                        "http://localhost:8081"
                 ));
                 configuration.setAllowedMethods(Collections.singletonList("*"));
                 configuration.setAllowCredentials(true);
